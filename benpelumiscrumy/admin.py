9143cd849b090cfaf1c3cb56072f5de567002cf6
Amuzu Imuse <Arete>
2020-05-13 08:38:31 +0100

    update update

--- benpelumiscrumy/admin.py
+++ benpelumiscrumy/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import ScrumyGoals
+from .models import ScrumyHistory
+from .models import GoalStatus
--- benpelumiscrumy/models.py
+++ benpelumiscrumy/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class GoalStatus(models.Model):
+    status_name = models.CharField(max_length = 30)
+
+class ScrumyGoals(models.Model):
+    goal_name = models.CharField(max_length = 30)
+    # goal_id 
+    created_by = models.CharField(max_length = 30)
+    moved_by = models.CharField(max_length = 30)
+    owner = models.CharField(max_length = 30)
+    goal_status = models.ForeignKey(
+        GoalStatus,
+        on_delete = models.PROTECT
+    ) 
+    user = models.ForeignKey(
+        User,
+        related_name = 'ScrumyGoals',
+        on_delete = models.PROTECT
+    )
+
+class ScrumyHistory(models.Model):
+    moved_by = models.CharField(max_length = 30)
+    created_by = models.CharField(max_length = 30)
+    moved_from = models.CharField(max_length = 50)
+    moved_to = models.CharField(max_length = 50)
+    time_of_action = models.DateField()
+    goal = models.ForeignKey(
+        ScrumyGoals,
+        on_delete = models.PROTECT
+    )
