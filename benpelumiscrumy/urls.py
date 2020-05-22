60c522a2874e12c4107ff3ca1790b71ec252dd88
Amuzu Imuse <Arete>
2020-05-22 06:48:16 +0100

    update

--- benpelumiscrumy/urls.py
+++ benpelumiscrumy/urls.py
@@ -14,10 +14,10 @@ from benpelumiscrumy import views
 urlpatterns = [
     # path('', homePage, name='home'),
      path( '', views.get_grading_parameters, name = 'index' ),
-    #  path('movegoal/<int:goal_id>', views.move_goal),
+     path('movegoal/<int:goal_id>', views.move_goal),
      path('addgoal/',views.add_goal),
      path('home/',views.home),
-    #  path('exception/', views.move_goal)
+     path('exception/', views.move_goal)
 ]
 
 
