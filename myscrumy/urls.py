02350a395f9fef45859becb38941fb885cd495d4
Amuzu Imuse <Arete>
2020-05-28 05:28:06 +0100

    update

--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('benpelumiscrumy/', include('benpelumiscrumy.urls')),
+    path ('benpelumiscrumyscrumy/', include('benpelumiscrumy.urls')),
     #  path ('' , include ('benpelumiscrumy.urls'))
 ]
 
