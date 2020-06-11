4ac0f6393542a64221802c9f33f3831115e20257
Amuzu Imuse <Arete>
2020-06-11 16:28:02 +0100

    update

--- websocket/urls.py
+++ websocket/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('connect/', views.connect, name='connect'),
     path('disconnect/', views.disconnect, name='disconnect'),
     path('send_message/', views.send_message, name= 'send_message'),
+    path('get_recent_messages/', views.get_recent_messages, name='get_recent_messages'),
  ]
